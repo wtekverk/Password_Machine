3b97f39b60ad1e260e063f16ca24ee668a222681
Wendy Tekverk <wtekverk@gmail.com>
2021-01-22 17:17:45 -0700

    added arrays of characters for passwords

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
+var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var specialCharacters = ["@", "%", "+", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~" ]
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+function generatePassword () {
+
+}
 
 // Write password to the #password input
 function writePassword() {
