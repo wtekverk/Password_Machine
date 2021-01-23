9f46f3366b0368e1c19585c2f8085982dd2d0f19
Wendy Tekverk <wtekverk@gmail.com>
2021-01-22 19:03:30 -0700

    added if statements to confirm whice arrays will end up in chosenCahracters

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"]
 
-var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"]
+// character sets
+var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
-var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
 
-var specialCharacters = ["@", "%", "+", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~" ]
+var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
+var specialCharacters = ["@", "%", "+", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~" ];
+
+// characters that user chose
+var chosenCharacters = [];
+
+//messages to user
 var characterQuantity = prompt("How many characters would you like your password to be? (between 8 and 123)")
 
 var characterLC = confirm("Would you like your password to include lower case letters?")
@@ -16,6 +22,13 @@ var characterN = confirm("Would you like your password to include numbers?")
 
 var characterSC = confirm("Would you like your password to include special characters?")
 
+// determining what characters will be chosen
+if (characterLC){chosenCharacters.push(lowerCase)}
+if (characterUP){chosenCharacters.push(upperCase)}
+if (characterN){chosenCharacters.push(numeric)}
+if (characterSC){chosenCharacters.push(specialCharacters)}
+
+console.log(chosenCharacters)
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -24,10 +37,15 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword () {
 
   
-  
+  for(var i=0; i < l; i++ )
+  {  
+  text += chosenCharacters.charAt(Math.floor(Math.random() * characterQuantity.length));
+  }
+  return text;
+  }
 
 
-}
+
 
 // Write password to the #password input
 function writePassword() {
