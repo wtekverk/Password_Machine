29dc338c66ce37a75a6a68febcecf3de5f003a08
Wendy Tekverk <wtekverk@gmail.com>
2021-01-23 17:14:15 -0700

    minor edits to form

--- script.js
+++ script.js
@@ -1,56 +1,67 @@
-
 // character sets to make full array 
 var lowerCase = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 var upperCase = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Z", "X", "C", "V", "B", "N", "M"];
 var numeric = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialCharacters = ["@", "%", "+", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~" ];
+var specialCharacters = ["@", "%", "+", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~"];
 
 
 
 var generateBtn = document.querySelector("#generate");
 
-function randomArray (array) {
-var Index= Math.floor(Math.random() * randomArray.length);
-return array[Index]
+function randomArray(array) {
+  var Index = Math.floor(Math.random() * randomArray.length);
+  return array[Index]
 }
 
-function generatePassword () {
+function generatePassword() {
+  //selecting characters for password 
+  var passwordString = []
+  var chosenCharacters = []
+  
+  //select length of password 
+  var characterQuantity = prompt("How many characters would you like your password to be? (between 8 and 128)")
 
-//select length of password 
-  var characterQuantity = prompt("How many characters would you like your password to be? (between 8 and 128)") 
-    
   if (characterQuantity < 8 || characterQuantity > 128) {
     alert("Please choose a valid number")
-return generatePassword ()}
+    return generatePassword()
+  }
 
-//boolean variables chosen by user 
+  //boolean variables chosen by user 
   var characterLC = confirm("Would you like your password to include lower case letters?");
   var characterUP = confirm("Would you like your password to include upper case letters");
   var characterN = confirm("Would you like your password to include numbers?");
   var characterSC = confirm("Would you like your password to include special characters?");
 
-  if (!characterLC&&!characterUP&&!characterN&&!characterSC) {
+  if (!characterLC && !characterUP && !characterN && !characterSC) {
     alert("Please select at least one type of character.")
-  return generatePassword ()}
+    return generatePassword()
+  }
 
-  var chosenCharacters = [];
 
-// determining what characters will be chosen after user checks booleans
-if (characterLC){chosenCharacters.push(lowerCase)}
-if (characterUP){chosenCharacters.push(upperCase)}
-if (characterN){chosenCharacters.push(numeric)}
-if (characterSC){chosenCharacters.push(specialCharacters)}
 
-//selecting characters for password 
-var passwordString = []
+  // determining what characters will be chosen after user checks booleans
+  if (characterLC) {
+    chosenCharacters.push(lowerCase)
+  }
+  if (characterUP) {
+    chosenCharacters.push(upperCase)
+  }
+  if (characterN) {
+    chosenCharacters.push(numeric)
+  }
+  if (characterSC) {
+    chosenCharacters.push(specialCharacters)
+  }
 
-while (passwordString.length < chosenCharacters.length) {
-  passwordString.push(randomArray(chosenCharacters))
+
+
+  while (passwordString.length < chosenCharacters.length) {
+    passwordString.push(randomArray(chosenCharacters))
+  }
+
+  return passwordString.join("")
 }
 
-return passwordString.join("")
-  }
-  
 
 
 // Write password to the #password input
